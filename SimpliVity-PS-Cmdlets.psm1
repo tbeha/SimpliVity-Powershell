7c30df2bf18d71d945c721e81540db2e14e172de
tbeha <38065422+tbeha@users.noreply.github.com>
2019-03-20 09:03:23 +0100

    Add files via upload
    Added License term to the file

--- SimpliVity-PS-Cmdlets.psm1
+++ SimpliVity-PS-Cmdlets.psm1
@@ -1,12 +1,24 @@
 ﻿<# 
 	.SYNOPSIS
-	OmniStack powershell cmdlets
-	v 2.0
-	(c) Thomas Beha, January 5, 2019
+	OmniStack powershell cmdlets  v 2.0
+	(Copyright) Thomas Beha, January 5, 2019
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+	
 	.DESCRIPTION
-	The cmdlets use the HPE SimpliVity 380 RestAPI: https://api.simplivity.com/
+	The cmdlets use the HPE SimpliVity 380 RestAPI: https://developer.hpe.com/api/simplivity/
 	.NOTES
-    The commands where tested on a SimpliVity System running OmniStack v 3.7.6U1
+    The commands where tested on a SimpliVity System running OmniStack v 3.7.6U1 and 3.7.7
 
 	The following functions are available:
 
